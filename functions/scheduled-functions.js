aa4d2884ee181a9ac8763dc8d87982775a61b5f8
SANDAY-JS <san.sh.s@i.softbank.jp>
2022-11-07 10:03:52 +0900

    tet

--- functions/scheduled-functions.js
+++ functions/scheduled-functions.js
@@ -3,7 +3,7 @@
 const { schedule } = require('@netlify/functions');
 
 const handler = async function(event, context) {
-    console.log("Received event:", event);
+    console.log("Received event on Root dir in functions only dir:", event);
 
     return {
         statusCode: 200,
